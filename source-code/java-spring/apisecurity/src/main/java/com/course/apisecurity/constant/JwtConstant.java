7fdba36bc8c077aa229fac2e6182b5137058caa9
holynekk <e238109@metu.edu.tr>
2023-10-06 20:37:06 +0300

    removed unnecessary part

--- source-code/java-spring/apisecurity/src/main/java/com/course/apisecurity/constant/JwtConstant.java
+++
@@ -1,7 +0,0 @@
-package com.course.apisecurity.constant;
-
-public interface JwtConstant {
-
-	String REQUEST_ATTRIBUTE_USERNAME = "request-username";
-
-}
--- source-code/java-spring/apisecurity/src/main/java/com/course/apisecurity/constant/RedisConstant.java
+++
@@ -1,7 +0,0 @@
-package com.course.apisecurity.constant;
-
-public interface RedisConstant {
-
-	String REQUEST_ATTRIBUTE_USERNAME = "request-username";
-
-}
