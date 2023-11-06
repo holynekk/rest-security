59430319f2bbe13deb856a7d8592bdf558cf0fa7
holynekk <e238109@metu.edu.tr>
2023-11-06 21:38:23 +0300

    logging and https configuration

---
+++ apisecurity/src/main/java/com/holynekk/apisecurity/api/filter/config/RequestLogFilterConfig.java
@@ -0,0 +1,20 @@
+package com.holynekk.apisecurity.api.filter.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
+
+//@Configuration
+public class RequestLogFilterConfig {
+
+    @Bean
+    public CommonsRequestLoggingFilter logFilter() {
+        CommonsRequestLoggingFilter filter = new CommonsRequestLoggingFilter();
+
+        filter.setIncludeQueryString(true);
+        filter.setIncludePayload(true);
+        filter.setIncludeHeaders(true);
+
+        return filter;
+    }
+}
